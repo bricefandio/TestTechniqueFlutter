23c4c6b5e1d8a7f698da59c8ef820c768db5d87b
fandiodieumoubricegeorgy <fandiodieumoubricegeorgy@gmail.com>
2025-11-30 18:09:12 +0000

    MAJ  RN_App

---
+++ rn_app/android/buildSrc/src/main/kotlin/com/facebook/react/RootProjectPlugin.kt
@@ -0,0 +1,10 @@
+package com.facebook.react
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class RootProjectPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        // No-op — required to satisfy "com.facebook.react.rootproject"
+    }
+}
